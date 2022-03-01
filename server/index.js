1542f9ecc4fda108ebabdccceb00b03d055e95ac
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-28 17:19:51 -0700

    maybe it was the period

--- server/index.js
+++ server/index.js
@@ -118,7 +118,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
-app.use(express.static(path.join(__dirname, "../")))
+app.use(express.static(path.join(__dirname, "../client")))
 
 
 // Dev 
