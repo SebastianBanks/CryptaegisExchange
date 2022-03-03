1034d42f72676b8ec5b09d8f114d659d4efb1c24
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-03-03 14:50:00 -0700

    forgot about some things, let's try this

--- client/app.js
+++ client/app.js
@@ -8,13 +8,7 @@ const itemSize = document.querySelector("#item_size")
 const ownerId = document.querySelector("#owner_id")
 const catId = document.querySelector("#cat_id")
 
-const coinbaseId = document.querySelector("#coinbaseId")
-const userForm = document.querySelector("#userForm")
-const userName = document.querySelector("#userName")
-const email = document.querySelector("#email")
-const phoneNum = document.querySelector("#phoneNum")
-const userLocation = document.querySelector("#location")
-const age = document.querySelector("#age")
+
 
 const searchField = document.querySelector("#searchField")
 const searchPrice = document.querySelector('#searchPrice')
@@ -90,25 +84,7 @@ const getAllItems = () => {
 }
 
 
-const createUser = (e) => {
-    e.preventDefault()
 
-    let body = {
-        coinbase_id: coinbaseId.value,
-        user_name: userName.value,
-        user_email: email.value,
-        user_phone_number: phoneNum.value,
-        user_location: userLocation.value,
-        user_age: age.value
-    }
-
-    axios.post(`${heroku}/createUser`, body)
-        .then(() => {
-            console.log("createUser-----------")
-            console.log(body)
-        })
-        .catch(err => console.log(err))
-}
 
 const createItem = async (e) => {
      e.preventDefault()
@@ -197,7 +173,7 @@ const getFilteredItems = (e) => {
 
 
 imageForm.addEventListener("submit", createItem)
-userForm.addEventListener("submit", createUser)
+
 searchBtn.addEventListener("click", getFilteredItems)
 
 
---
+++ client/form.js
@@ -0,0 +1,32 @@
+const coinbaseId = document.querySelector("#coinbaseId")
+const userForm = document.querySelector("#userForm")
+const userName = document.querySelector("#userName")
+const email = document.querySelector("#email")
+const phoneNum = document.querySelector("#phoneNum")
+const userLocation = document.querySelector("#location")
+const age = document.querySelector("#age")
+
+const localHost = "http://localhost:3000"
+const heroku = "https://cryptaegis-exchange.herokuapp.com"
+
+const createUser = (e) => {
+    e.preventDefault()
+
+    let body = {
+        coinbase_id: coinbaseId.value,
+        user_name: userName.value,
+        user_email: email.value,
+        user_phone_number: phoneNum.value,
+        user_location: userLocation.value,
+        user_age: age.value
+    }
+
+    axios.post(`${heroku}/createUser`, body)
+        .then(() => {
+            console.log("createUser-----------")
+            console.log(body)
+        })
+        .catch(err => console.log(err))
+}
+
+userForm.addEventListener("submit", createUser)
