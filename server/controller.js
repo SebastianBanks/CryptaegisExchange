7477829ea5b8b108205fe2b0c41a56cd0fac9f3e
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-03-05 16:01:19 -0700

    let's hope this does what I want it to

--- server/controller.js
+++ server/controller.js
@@ -485,27 +485,26 @@ module.exports = {
             SELECT coinbase_connect_user_id FROM coinbase_connect
         `)
         .then(coinbase_id => {
-            console.log(coinbase_id[0][0]["coinbase_connect_user_id"])
-            console.log(encryptedId)
+            if (decrypt(coinbase_id, CRYPTO_SECERET) !== decrypt(encryptedId, CRYPTO_SECERET)) {
+                console.log(`1st: ${coinbase_id[0][0]["coinbase_connect_user_id"]}`)
+                console.log(`2nd: ${encryptedId}`)
 
-            sequelize.query(`
-            INSERT INTO user_account(user_name, user_email, user_phone_number, user_location)
-            VALUES('${encryptedUserName}', '${encryptedUserEmail}', '${encryptedUserPhoneNumber}', '${encryptedUserLocation}')
-            RETURNING user_id;
-            `)
-            .then(user_id => {
-                currentUser = user_id[0][0]["user_id"]
-                console.log(`currentUser: ${currentUser}`)
                 sequelize.query(`
-                    INSERT INTO coinbase_connect(coinbase_connect_user_id, user_id)
-                    VALUES('${encryptedId}', ${user_id[0][0]["user_id"]});
+                    INSERT INTO user_account(user_name, user_email, user_phone_number, user_location)
+                    VALUES('${encryptedUserName}', '${encryptedUserEmail}', '${encryptedUserPhoneNumber}', '${encryptedUserLocation}')
+                    RETURNING user_id;
                 `)
-                .then(res => {
-                    res.redirect('/')
-                })
+                .then(user_id => {
+                    currentUser = user_id[0][0]["user_id"]
+                    console.log(`currentUser: ${currentUser}`)
+                    sequelize.query(`
+                        INSERT INTO coinbase_connect(coinbase_connect_user_id, user_id)
+                        VALUES('${encryptedId}', ${user_id[0][0]["user_id"]});
+                    `)
+                    .then(res.redirect("/"))
                 
             })
-        
+            }
         })
 
     },
