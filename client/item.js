1bc86821da6953d5150efd927f87ffa2741976f4
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-03-07 11:06:24 -0700

    testing

--- client/item.js
+++ client/item.js
@@ -115,6 +115,77 @@ const goBackAPage = (e) => {
     window.location = "/"
 }
 
+const createEditDiv = async (item) => {
+    const itemName = await convertAlteredString(item["item_title"])
+    const itemCost = item["item_price"]
+    const itemId = item["item_id"]
+    const itemCat = await convertAlteredString(item["category_id"])
+    const itemDesc = await convertAlteredString(item["item_description"])
+    const itemSize = awaitconvertAlteredString(item["item_size"])
+    const isAvailable = item["item_is_available"]
+    
+
+    let imageUrl = await getImageUrl(itemId).then(res => {
+        console.log(res)
+        return res
+    })
+    console.log(getImageUrl(itemId))
+
+    const editDiv = 
+        `
+            <div class="editItem" id="${itemId}">
+                <img id="editImageForm" src="${imageUrl}"/>
+                <div class="editForm">
+                    <form class="editFormProperties">
+                    <input id="imageInput" type="file" accept="image/*" multiple required>
+                    <input id="item_price" type="number" placeholder="Item price" step=0.01 value="${itemCost}" required>
+                    <input id="item_title" type="text" placeholder="Item title" value="${itemName}" required>
+                    <textarea id="item_desc" rows="10" cols="30" placeholder="Item Description" style="color: black;" value="${itemDesc}" required></textarea> 
+                    <input id="item_size" type="text" placeholder="Item Size" value="${itemSize}" required>
+                    <select id="cat_id" placeholder="Category" value="${itemCat}" required>
+                        <option value="0" disabled selected>Select a Category</option>
+                        <option value="1">Appliances</option>
+                        <option value="2">Art</option>
+                        <option value="3">Auto Parts & Accessories</option>
+                        <option value="4">Baby</option>
+                        <option value="5">Books & Media</option>
+                        <option value="6">Clothings & Appareal</option>
+                        <option value="19">Cookware</option>
+                        <option value="7">Cycling</option>
+                        <option value="8">Electronics</option>
+                        <option value="9">Fitness Equipment</option>
+                        <option value="10">Furniture</option>
+                        <option value="11">General</option>
+                        <option value="12">Home & Garden</option>
+                        <option value="13">Hunting & Fishing</option>
+                        <option value="14">Musical Instruments</option>
+                        <option value="15">Outdoors & Sporting</option>
+                        <option value="16">Pets</option>
+                        <option value="17">Recreational Vehicles</option>
+                        <option value="18">Toys</option>  
+                    </select>
+                    <input type="checkbox" value="Item is available" checked>
+                    <button type="submit">Update</button>
+                    <button type="submit">Cancel</button>
+                    </form>
+                </div>
+            </div>
+        `
+    return editDiv
+}
+
+const editItem = (e) => {
+    axios.get(`${heroku}/itemPage/${itemId}`)
+    .then(res => {
+        res.data.forEach(async item => {
+            console.log(await item)
+            const editForm = await createEditDiv(item)
+            body.innerHTML += editForm
+        })
+    })
+}
+
 getItemDesc()
 canEditBtn()
 backBtn.addEventListener("click", goBackAPage)
+editBtn.addEventListener("click", editItem)
