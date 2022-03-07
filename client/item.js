4767db8b3630dc739d10a5dfc5ab192a3b35ef27
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-03-07 12:19:49 -0700

    hopefully this makes things better aligned

--- client/item.js
+++ client/item.js
@@ -151,7 +151,7 @@ const createEditDiv = async (item) => {
                     <p class="editPropTitle">Price:</p>
                     <input id="priceEdit" type="number" placeholder="Item price" step=0.01 value="${itemCost}" required>
                     <p class="editPropTitle">Description:</p>
-                    <textarea id="descEdit" rows="7" cols="20" placeholder="Item Description" style="color: black;" value="${itemDesc}" required></textarea> 
+                    <textarea id="descEdit" rows="4" cols="20" placeholder="Item Description" style="color: black;" value="${itemDesc}" required></textarea> 
                     <p class="editPropTitle">Size:</p>
                     <input id="sizeEdit" type="text" placeholder="Item Size" value="${itemSize}" required>
                     <select id="catEdit" placeholder="Category" value="${itemCat}" required>
@@ -176,9 +176,12 @@ const createEditDiv = async (item) => {
                         <option value="17">Recreational Vehicles</option>
                         <option value="18">Toys</option>  
                     </select>
+                    <p class="editPropTitle">Is available:</p>
                     <input id="checkEdit" type="checkbox" value="Item is available" checked>
-                    <button class="editItemButton" type="submit">Update</button>
-                    <button class="editItemButton" type="submit">Cancel</button>
+                    <div class="editButtons">
+                        <button class="editItemButton" type="submit">Update</button>
+                        <button class="editItemButton" type="submit">Cancel</button>
+                    </div>
                     </form>
                 </div>
             </div>
